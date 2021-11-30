67d29313da0d7b3add438081b9e73290cbfe0c63
Islom <b.madiyorov.i@mail.ru>
2021-11-30 19:06:09 +0500

    test

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let chislo = +prompt ('Введите число');
+let stepen = +prompt ('Введите степень');
+let itog;
+
+for (let i = 0; i < 1; i++) {
+    itog = chislo ** stepen
+ 
+    // alert ('Число в степени ' + itog);
+    console.log ('Число в степени ' + itog);
+}
+
+//   do {
+//     console.log(chislo);
+//     i++
+// }while(isNaN(chislo) || chislo != chislo){
+//     console.log(chislo);
+// }
