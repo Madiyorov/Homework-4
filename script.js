1a86a1f9d6acd16078667f726b4aa538744812b7
Islom <b.madiyorov.i@mail.ru>
2021-12-03 18:45:46 +0500

    test3

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+/* ДЗ-№4 */
 
-let chislo = +prompt ('Введите число');
-let stepen = +prompt ('Введите степень');
-let itog;
+                    /* №-1 */
 
-for (let i = 0; i < 1; i++) {
-    itog = chislo ** stepen
- 
-    // alert ('Число в степени ' + itog);
-    console.log ('Число в степени ' + itog);
+do {
+    var num = +prompt('Введите число')
+}while(isNaN(num) || num == 0)
+
+do {
+    var stp = +prompt('Введите степень')
+}while (isNaN(stp) || stp == 0 )
+
+let answer = 1;
+
+for(let i = 0; i < stp; i++) {
+    answer = answer * num
 }
 
-//   do {
-//     console.log(chislo);
-//     i++
-// }while(isNaN(chislo) || chislo != chislo){
-//     console.log(chislo);
-// }
+console.log(answer);
+
+                    /* №-2 */
+
+do{
+    var num = +prompt('Введите кол-во ступенек');
+}while(isNaN(num) || num <= 0) 
+
+do{
+    var sym = prompt('Введите конечный символ');
+}while(sym == '')
+
+do{
+    var finalSym = prompt('Введите конечный символ');
+}while(finalSym == '')
+
+for(let i = 0; i < num; i++) {
+    finalSym = sym + finalSym
+    console.log(finalSym);
+}
